50797a7ec89bd2378e77bf62916373bab50f8f6f
Hyt <69683382@qq.com>
2022-04-26 21:37:25 +0800

    feat: 学生表的普通属性的更新
    顺带加个 dirty 标志, 来回换了一堆事件才找到合适的
    随便排了个版, 能跑就行

--- StudentManagerSystem/Entities/Student.cs
+++ StudentManagerSystem/Entities/Student.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace StudentManageSystem.Entities
 {
@@ -7,11 +8,49 @@ namespace StudentManageSystem.Entities
     /// </summary>
     public class Student
     {
-        public string Id { get; set; }
+        public string Id { get; set; } = "";
         public string Name { get; set; }
         public DateTime Birth { get; set; }
         public bool IsMale { get; set; }
+        [NotMapped]
+        public string Gender
+        {
+            get => IsMale ? "男" : "女";
+            set
+            {
+                switch (value.Trim())
+                {
+                    case "男":
+                        IsMale = true;
+                        break;
+                    case "女":
+                        IsMale = false;
+                        break;
+                }
+            }
+        }
         public int ClassId { get; set; }
         public NaturalClass Class { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            if(obj is Student s)
+            {
+                return Equals(s);
+            }
+
+            return false;
+        }
+
+        protected bool Equals(Student other)
+        {
+            return Id == other.Id && Name == other.Name && Birth.Equals(other.Birth) && IsMale == other.IsMale && ClassId == other.ClassId;
+        }
+
+        public override int GetHashCode()
+        {
+            // ReSharper disable once NonReadonlyMemberInGetHashCode
+            return HashCode.Combine(Id, Name, Birth, IsMale, ClassId);
+        }
     }
 }
--- StudentManagerSystem/MainWindow.xaml.cs
+++ StudentManagerSystem/MainWindow.xaml.cs
@@ -1,9 +1,14 @@
-using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
 using Microsoft.EntityFrameworkCore;
 using StudentManageSystem.DataBase;
+using StudentManageSystem.Entities;
 
 namespace StudentManageSystem
 {
@@ -12,19 +17,69 @@ namespace StudentManageSystem
     /// </summary>
     public partial class MainWindow : Window
     {
-        public StudentDataBase StudentDbContext { get; }
+
+        public const string BasicTitle = "学生管理系统";
+        /// <summary>
+        /// 学生数据库
+        /// </summary>
+        public StudentDataBase StudentDataBase => _studentDbContext ??= new StudentDataBase();
+        public bool DataBaseDirty
+        {
+            get => _dataBaseDirty;
+            set
+            {
+                _dataBaseDirty = value;
+                Title = _dataBaseDirty ? $"{BasicTitle}*" : BasicTitle;
+            }
+        }
+
+        /// <summary>
+        /// 学生视图表
+        /// </summary>
+        public CollectionViewSource studentViewSource;
+
+        private bool _dataBaseDirty;
+        private StudentDataBase? _studentDbContext;
+        private ICollection<Student>? _studentDataSource;
 
         public MainWindow()
         {
-            StudentDbContext = new StudentDataBase();
-            Debug.WriteLine(StudentDbContext.DbPath);
-            StudentDbContext.Database.Migrate();
+            Debug.WriteLine("/---------------------------------/\n" +
+                            "|StudentManageSystem\n" +
+                            "|State: Before MainWindow.Ctor\n" +
+                            "|Detail: component not initialized.\n" +
+                            "/---------------------------------/");
+
             InitializeComponent();
+            // 获取或设置XAML资源
+            studentViewSource = (CollectionViewSource)FindResource(nameof(studentViewSource));
+            DataBaseDirty = false;
+            studentGender.ItemsSource = new[] { "男", "女" };
+
+            // 初始化数据库
+            StudentDataBase.Database.Migrate();
+            StudentDataBase.Students.Load();
+
+            // 绑定数据源
+            _studentDataSource = StudentDataBase.Students.Local.ToObservableCollection();
+            studentViewSource.Source = _studentDataSource;
+            
         }
 
-        ~MainWindow()
+        protected override void OnClosing(CancelEventArgs e)
         {
-            StudentDbContext.Dispose();
+            _studentDbContext!.Dispose();
+        }
+
+        private void SaveChangesButton_Click(object sender, RoutedEventArgs e)
+        {
+            StudentDataBase.SaveChanges();
+            DataBaseDirty = false;
+        }
+        
+        private void studentsDataGrid_CurrentCellChanged(object sender, System.EventArgs e)
+        {
+            if (StudentDataBase.ChangeTracker.HasChanges()) DataBaseDirty = true;
         }
     }
 }
